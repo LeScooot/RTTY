dd2d9f39c962f8627f87f2cd23bb0c1bbfe3a73f
LeScooot <jeronimus.ryan@gmail.com>
2025-02-04 13:54:41 -0500

    tone

--- Arduino Scripts/RTTY_Transmitter/RTTY_Transmitter.ino
+++ Arduino Scripts/RTTY_Transmitter/RTTY_Transmitter.ino
@@ -1,7 +1,9 @@
 const int IR = 9;
-const int high_freq = 2125;
-const int low_freq = 2295;
-const int
+const unsigned int high_freq = 2125;
+const unsigned int low_freq = 2295;
+const double timing 1/(45.45);
+
+int counter = 0;
 
 void setup() {
   pinmode(IR, OUTPUT);
@@ -11,8 +13,14 @@ void setup() {
 void square_wave(int pin, int frequency);
 
 void loop() {
-  square_wave(IR, high_freq);
-
+  if(counter%2 == 0){
+    square_wave_tone(high_freq);
+  }
+  else{
+    square_wave_tone(low_freq);
+  }
+  delay(timing * 1000);
+  counter++
 }
 
 void square_wave(int pin, int frequency){
@@ -24,10 +32,16 @@ void square_wave(int pin, int frequency){
   deigitalWrite(pin, LOW);
   delay(period/2);
 }
+
+void square_wave_tone(int frequency){
+  tone(IR, frequency);
+}
+
 void testing_function(){
   square_wave(IR, high_freq);
   delay(50)
 }
+
 void string_to_binary(int& string, int& binary){
   char character;
   byte b;
@@ -37,7 +51,7 @@ void string_to_binary(int& string, int& binary){
     for(int i = 0; i < 7; i++){
       c = bitread(character, i);
 
-      
+
     }
     
   }
